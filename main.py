4405d247bb25d0ca6245d65eb327b40a3177aacc
IsaacVerbrugge25 <personontheinternet1234@gmail.com>
2024-04-16 15:22:47 -1000

    scoring, yes / no game buttons

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import enum
 import struct
 
 import pygame
@@ -29,6 +30,10 @@ def draw_button(text, font, color, rect):
     pygame.draw.rect(screen, BLACK, rect, 2)
     draw_text(text, font, color, rect.centerx, rect.centery)
 
+class QuestionTypes(enum.Enum):
+    PHISH = 1
+    NOT_A_PHISH = 2
+
 # Main welcome page loop
 def welcome_page(highscore):
     running = True
@@ -37,15 +42,15 @@ def welcome_page(highscore):
         screen.fill(WHITE)
 
         # Draw title
-        draw_text("CyberSafe Seniors", FONT, BLACK, WIDTH // 2, HEIGHT // 3)
+        draw_text("CyberSafe Seniors", FONT, BLACK, WIDTH / 2, HEIGHT / 3)
 
         # Draw instruction
-        draw_text("Click below to start the game.", FONT, BLACK, WIDTH // 2, HEIGHT // 2)
+        draw_text("Click below to start the game.", FONT, BLACK, WIDTH / 2, HEIGHT / 2)
 
-        draw_text(f"Previous Highscore: {highscore}", FONT, BLACK, WIDTH // 2, HEIGHT - 15)
+        draw_text(f"Previous Highscore: {highscore}", FONT, BLACK, WIDTH / 2, HEIGHT - 15)
 
         # Draw button
-        button_rect = pygame.Rect(WIDTH // 3, HEIGHT * 2 // 3, WIDTH // 3, HEIGHT // 6)
+        button_rect = pygame.Rect(WIDTH / 3, HEIGHT * 2 / 3, WIDTH / 3, HEIGHT / 6)
         draw_button("Game", FONT, BLACK, button_rect)
 
         # Update the display
@@ -68,15 +73,21 @@ def game_page(highscore):
 
     while running:
         screen.fill(WHITE)
+        true_answer = QuestionTypes.PHISH
 
         # display images
-        image = pygame.transform.scale(pygame.image.load("resources/mountain.jpeg"), ((WIDTH // 2) - WIDTH // 16, HEIGHT // 2 - WIDTH // 16))
-        
-        draw_text("Option 1", FONT, BLACK, (WIDTH// 4), (HEIGHT//4) - 40)
-        screen.blit(image, ((WIDTH// 32), HEIGHT//4))
+        image = pygame.transform.scale(pygame.image.load("resources/mountain.jpeg"), ((WIDTH / 2) - WIDTH / 16, HEIGHT / 2 - WIDTH / 16))
+        draw_text("Is this a phishing attempt?", FONT, BLACK, (WIDTH / 2), (HEIGHT/4) - 40)
+        screen.blit(image, ((WIDTH / 2) - (image.get_width() / 2), HEIGHT/4))
+        draw_text(f"Score: {score}", FONT, BLACK, (WIDTH - 100), 20)
 
-        draw_text("Option 2", FONT, BLACK, (WIDTH// 2 + WIDTH//4), (HEIGHT//4) - 40)
-        screen.blit(image, (WIDTH//2 + WIDTH//32, HEIGHT//4))
+        # Draw button
+        button_yes = pygame.Rect(WIDTH / 5 - (WIDTH / 3)/2, HEIGHT * 5 / 6, WIDTH / 3, HEIGHT / 7)
+        draw_button("Yes", FONT, BLACK, button_yes)
+
+        button_no = pygame.Rect(WIDTH - (WIDTH / 5) - (WIDTH / 3)/2, HEIGHT * 5 / 6, WIDTH / 3, HEIGHT / 7)
+        draw_button("No", FONT, BLACK, button_no)
+
         # Update the display
         pygame.display.flip()
 
@@ -85,6 +96,21 @@ def game_page(highscore):
             if event.type == pygame.QUIT:
                 running = False
                 return score if score > highscore else highscore
+            elif event.type == pygame.MOUSEBUTTONDOWN:
+                x, y = pygame.mouse.get_pos()
+
+                if button_yes.collidepoint(x, y):
+                    if true_answer == QuestionTypes.NOT_A_PHISH:
+                        print("loss")
+                    elif true_answer == QuestionTypes.PHISH:
+                        score += 1
+                elif button_no.collidepoint(x, y):
+                    if true_answer == QuestionTypes.NOT_A_PHISH:
+                        score += 1
+                    if true_answer == QuestionTypes.PHISH:
+                        print("loss")
+
+
 
 # Main function to control the flow of the program
 def main():
@@ -93,7 +119,7 @@ def main():
     if(os.path.exists("playerdata/playerdata.dat")):
         with open("playerdata/playerdata.dat", 'r') as file:
             lines = file.readlines()
-            if lines[0] != "":
+            if len(lines) != 0:
                 highscore = int(lines[0])
 
     next_page = welcome_page(highscore)
